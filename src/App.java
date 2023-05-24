401b9abdf4dbeac8bf5f2f211d95d50302818380
farrelraihan <113604965+farrelraihan@users.noreply.github.com>
2023-05-24 10:30:53 +0700

    Update App.java
    Menambahkan Print Oshi No Ko

--- src/App.java
+++ src/App.java
@@ -2,5 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, woeee!");
 
+        System.out.println("Oshi No Ko");
+
     }
 }
